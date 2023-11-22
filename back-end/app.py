991628cf4f7d06697617968a93c13bc4634a85ff
MOHAMMAD IQBAL MAULANA <firmanpeman@gmail.com>
2023-11-22 09:40:33 +0700

    membuat endpoint chatbot minQ

--- back-end/app.py
+++ back-end/app.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, jsonify
 import pickle
 from PyPDF2 import PdfReader
-from sklearn.feature_extraction.text import TfidfVectorizer
+import json
 import re
+import string
+import random
+import numpy as np
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
+
 # Fungsi untuk membaca konten dari file PDF
 def read_pdf(file_path):
     pdf = PdfReader(file_path)
@@ -13,15 +19,7 @@ def read_pdf(file_path):
         text += page.extract_text()
     return text
 
-# Endpoint GET untuk membaca dan mengembalikan data dari file PDF
-@app.route('/get_data', methods=['GET'])
-def get_pdf_data():
-    file_path = 'mycv.pdf' 
-    pdf_content = read_pdf(file_path)
-    response_data = {'pdf_content': pdf_content}
-
-    return jsonify(response_data)
-
+# Fungsi untuk membersihkan teks
 def cleanResume(txt):
     cleanText = re.sub('http\S+\s', ' ', txt)
     cleanText = re.sub('RT|cc', ' ', cleanText)
@@ -32,11 +30,20 @@ def cleanResume(txt):
     cleanText = re.sub('\s+', ' ', cleanText)
     return cleanText
 
-with open('model/clf.pkl', 'rb') as clf_file:
+# Endpoint GET untuk membaca dan mengembalikan data dari file PDF
+@app.route('/get_data', methods=['GET'])
+def get_pdf_data():
+    file_path = 'mycv.pdf' 
+    pdf_content = read_pdf(file_path)
+    response_data = {'pdf_content': pdf_content}
+
+    return jsonify(response_data)
+
+with open('model/catagory/clf.pkl', 'rb') as clf_file:
     clf = pickle.load(clf_file)
 
 
-with open('model/tfidf.pkl', 'rb') as tfidf_file:
+with open('model/catagory/tfidf.pkl', 'rb') as tfidf_file:
     tfidf = pickle.load(tfidf_file)
 
 category_mapping = {
@@ -111,6 +118,49 @@ def predict():
         predicted_percentage = int(predicted_percentage)
 
         return jsonify({'predicted_percentage': predicted_percentage})
+    
+
+    # Load tokenizer, label encoder, input shape, and model
+with open('model/minQ/tokenizers.pkl', 'rb') as tokenizers_file:
+    tokenizer = pickle.load(tokenizers_file)
+
+with open('model/minQ/le.pkl', 'rb') as labelencoder_file:
+    le = pickle.load(labelencoder_file)
+
+with open('model/minQ/input_shape.pkl', 'rb') as inputshape_file:
+    input_shape = pickle.load(inputshape_file)
+
+model = load_model('model/minQ/chat_model.h5')
+
+with open('dataset/Screning_CV.json') as content:
+    data1 = json.load(content)
+
+responses = {}
+for intent in data1['intents']:
+    responses[intent['tag']] = intent['responses']
+
+# endpoimd chatbot minQ
+@app.route('/minQ', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_input = data['user_input']
+
+    texts_p = []
+    prediction_input = [letters.lower() for letters in user_input if letters not in string.punctuation]
+    prediction_input = ''.join(prediction_input)
+    texts_p.append(prediction_input)
+
+    prediction_input = tokenizer.texts_to_sequences(texts_p)
+    prediction_input = np.array(prediction_input).reshape(-1)
+    prediction_input = pad_sequences([prediction_input], input_shape)
+
+    output = model.predict(prediction_input)
+    output = output.argmax()
+
+    response_tag = le.inverse_transform([output])[0]
+    response = random.choice(responses.get(response_tag, ["Maaf, minQ tidak mengerti itu."]))
+
+    return jsonify({'response': response})
 
 if __name__ == '__main__':
     app.run(debug=True)
